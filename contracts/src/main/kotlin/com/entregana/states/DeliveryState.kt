c468b0a59cf6bcf22e0c190ecdd1819eee6e012d
Hristo Gatsinski <gatsinski@gmail.com>
2020-10-15 22:24:06 +0300

    Add DeliveryState

--- contracts/src/main/kotlin/com/entregana/states/DeliveryState.kt
+++ contracts/src/main/kotlin/com/entregana/states/DeliveryState.kt
@@ -2,11 +2,31 @@ package com.entregana.states
 
 import com.entregana.contracts.DeliveryContract
 import net.corda.core.contracts.BelongsToContract
-import net.corda.core.contracts.ContractState
-import net.corda.core.identity.AbstractParty
+import net.corda.core.contracts.LinearState
+import net.corda.core.contracts.UniqueIdentifier
+import net.corda.core.identity.Party
+import net.corda.core.serialization.CordaSerializable
 
 // *********
 // * State *
 // *********
 @BelongsToContract(DeliveryContract::class)
-data class DeliveryState(val data: String, override val participants: List<AbstractParty> = listOf()) : ContractState
+data class DeliveryState(
+        val packageType: String,
+        val sender: Party,
+        val courier: Party,
+        val recipient: Recipient,
+        val status: String,
+        override val linearId: UniqueIdentifier = UniqueIdentifier(),
+        override val participants: List<Party> = listOf(sender, courier)
+) : LinearState
+
+@CordaSerializable
+data class Recipient(
+    val firstName: String,
+    val lastName: String,
+    val address: String,
+    val email: String,
+    val phone: String,
+    val additionalDetails: String = ""
+)
